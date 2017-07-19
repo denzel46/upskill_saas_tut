44ca6ccbb075a7f23cf79dd61e0f6f50a49b1797
Dennis Wallraff <wallraff.d@gmail.com>
2017-07-19 15:24:51 +0000

    Added Mailer controller

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,4 +1,4 @@
-class COntactMailer < ActionMailer::Base
+class ContactMailer < ActionMailer::Base
     default to: 'wallraff.d@gmail.com'
     def contact_email(name, email, body)
         @name = name
